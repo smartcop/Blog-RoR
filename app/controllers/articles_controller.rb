a48ec972d1b9c6ca29590a784a7a0ff67f6ccc1e
Mark Novak <mnovak@mit.edu>
2016-05-21 15:37:28 -0400

    Deleting Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,16 +3,16 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
-
   def show
     @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -27,11 +27,19 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
+
     if @article.update(article_params)
       redirect_to @article
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
